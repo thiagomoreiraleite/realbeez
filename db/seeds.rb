e483cefec33c2b31ef4a5e4d4babee485b7ae0f0
steven ly <shiwenly@gmail.com>
2019-09-29 20:21:41 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,142 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Candidature.delete_all
+Availability.delete_all
+Meeting.delete_all
+Annonce.delete_all
+Agent.delete_all
+User.delete_all
+
+User.create!(
+  email: 'shiwenly@gmail.com',
+  password: '123456',
+  civilité: 'Monsieur',
+  nom: 'Ly',
+  prénom: 'steven',
+  adresse: '90 avenue de Choisy, paris',
+  ville: 'Paris',
+  téléphone: '0642808389',
+  statut: 'Agent',
+  admin: true
+)
+
+User.create!(
+  email: 'ly.somchit@gmail.com',
+  password: '123456',
+  civilité: 'Madame',
+  nom: 'Ly',
+  prénom: 'christine',
+  adresse: '110 rue saint-jacques, thann',
+  ville: 'Thann',
+  téléphone: '0651402566'
+)
+
+User.create!(
+  email: 'ly8thierry@gmail.com',
+  password: '123456',
+  civilité: 'Monsieur',
+  nom: 'Ly',
+  prénom: 'thierry',
+  adresse: '75 rue patay, paris',
+  ville: 'Paris',
+  téléphone: '+447710168923',
+  statut: 'Agent'
+)
+
+User.create!(
+  email: 'ly8david@gmail.com',
+  password: '123456',
+  civilité: 'Monsieur',
+  nom: 'Ly',
+  prénom: 'David',
+  adresse: '75 rue patay, paris',
+  ville: 'Paris',
+  téléphone: '+971561216797'
+)
+
+User.create!(
+  email: 'immolaurent68@gmail.com',
+  password: '123456',
+  civilité: 'Monsieur',
+  nom: 'Uhlen',
+  prénom: 'Laurent',
+  adresse: '10 Rue De Pfastatt, 68460 Lutterbach',
+  ville: 'Lutterbach',
+  téléphone: '0666774000',
+  statut: 'Agent'
+)
+
+User.create!(
+  email: 'sherleylueis@gmail.com',
+  password: '123456',
+  civilité: 'Madame',
+  nom: 'Lueis',
+  prénom: 'Sherley',
+  adresse: '20, Rue du Jeu des Enfants, 67000 Strasbourg',
+  ville: 'Strasbourg',
+  téléphone: '0623894028'
+)
+
+Annonce.create!(
+  titre_annonce: 'Charmant F3 au coeur de Riedisheim (parking disponible)',
+  type_de_bien: 'Appartement',
+  meublé: 'Non meublé',
+  surface: '60',
+  pièces: '3',
+  description:
+  'Appartement de type F3, au RDC (surélevé) d’un immeuble calme de 2 étages, se composant d’un séjour, d’une cuisine équipée, de 2 chambres, d’un WC et d’une salle de bain.
+  Place de parking disponible dans la cour de l’immeuble.
+
+  Disponible immédiatement, aucun frais d’agence.
+  Loyer 560 euros et 50 euros de provision pour charges (garant exigé)',
+  loyer_mensuel: 610,
+  adresse: '5 rue de la paix, Riedisheim',
+  ville: 'Riedisheim',
+  email: 'shiwenly@gmail.com',
+  téléphone: '0642808389',
+  user_id: 1
+)
+
+Annonce.create!(
+  titre_annonce: 'Local commercial de 52 m2',
+  type_de_bien: 'Autre',
+  meublé: 'Non meublé',
+  surface: '52',
+  pièces: '2',
+  description:
+  'A louer local commercial de 52 m2 situé proche centre-ville d’Altkirch avec place de parkings juste en face.
+Il comprend une belle pièce principale pouvant faire office d espace de vente, une seconde pièce à l’arrière ainsi qu’un w.c séparé et une petite cuisine.
+
+loyer 600 euros HT et 20 euros de provision pour charges.
+
+Disponible immédiatement et pas de frais d’agence à payer.',
+  loyer_mensuel: 620,
+  adresse: '9 rue de Carspach, Altkirch',
+  ville: 'Altkirch',
+  email: 'shiwenly@gmail.com',
+  téléphone: '0642808389',
+  user_id: 1
+)
+
+Annonce.create!(
+  titre_annonce: 'Proche musée, studio au rez-de-chaussée',
+  type_de_bien: 'Appartement',
+  meublé: 'Non meublé',
+  surface: '30',
+  pièces: '1',
+  description:
+  'Thann proche musée,
+Studio à louer, au rez-de-chaussée d un petit immeuble calme.
+Il comprend une pièce principale, une cuisine aménagée et une salle de bain avec WC.
+
+Loyer: 260 €
+Charges: 40€ (eau froide, entretien chaudière + commun)',
+  loyer_mensuel: 300,
+  adresse: '36 rue de la Halle, Thann',
+  ville: 'Thann',
+  email: 'ly.somchit@gmail.com',
+  téléphone: '0651402566',
+  user_id: 2
+)
