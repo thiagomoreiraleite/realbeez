302903c41761fdb25d71f54b4a4a524cca36022b
steven ly <shiwenly@gmail.com>
2019-10-29 21:35:48 +0700

    change home page annonces

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :details_tarifs]
 
   def home
-
     if user_signed_in? && current_user.ville != nil
       ville = current_user.ville
       if Annonce.where("statut = ?", "active" ).near("#{ville}",30) == []
-        @annonces = Annonce.where("statut = ?", "active").near('paris',30).limit(3)
+        @annonces = Annonce.where("statut = ?", "active").order(created_at: :desc).limit(3)
       else
         @annonces = Annonce.where("statut = ?", "active" ).near("#{ville}",30).limit(3)
       end
@@ -16,7 +15,7 @@ class PagesController < ApplicationController
       #   @profiles = Profile.where("statut = ?", "Agent" ).near(current_user.adresse,100).limit(4)
       # end
     else
-      @annonces = Annonce.where("statut = ?", "active").near('paris',30).limit(3)
+      @annonces = Annonce.where("statut = ?", "active").order(created_at: :desc).limit(3)
       # @profiles = Profile.where("statut = ?", "Agent").limit(4)
     end
   end
