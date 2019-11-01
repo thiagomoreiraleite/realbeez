a311663393fc36bb708b083b5c33a57d6faca14e
steven ly <shiwenly@gmail.com>
2019-11-01 18:52:33 +0800

    change url in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "http://realbeez.com" }
+  config.action_mailer.default_url_options = { host: "realbeez.herokuapp.com" }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
