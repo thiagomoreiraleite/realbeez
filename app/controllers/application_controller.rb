d5252e6d4c806c06da36e5addb34634ccaccbd4b
steven ly <shiwenly@gmail.com>
2019-11-12 18:09:00 +0100

    ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  force_ssl
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exceptions
