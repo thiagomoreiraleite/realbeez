0cc4a4708b630b7f558db79cb11edaf964979ef1
steven ly <shiwenly@gmail.com>
2020-01-10 10:44:03 +0100

    change profile controller for home page to stop searching by location

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,22 +2,22 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :details_tarifs, :mentions_legales, :nous_decouvrir, :fiches_pratiques, :statut_auto_entrepreneur, :new, :create, :reponse_contactez_nous]
 
   def home
-    if user_signed_in? && current_user.ville != nil
-      ville = current_user.ville
-      if Annonce.where("statut = ?", "active" ).near("#{ville}",30) == []
-        @annonces = Annonce.where("statut = ?", "active").order(created_at: :desc).limit(3)
-      else
-        @annonces = Annonce.where("statut = ?", "active" ).near("#{ville}",30).limit(3)
-      end
-      if Profile.where("statut = ?", "Agent" ).near("#{ville}",50) == []
-        @profiles = Profile.where("statut = ?", "Agent").limit(10)
-      else
-        @profiles = Profile.where("statut = ? AND id != ?", "Agent", current_user.id ).near("#{ville}",50).limit(10)
-      end
-    else
-      @annonces = Annonce.where("statut = ?", "active").order(created_at: :desc).limit(3)
-      @profiles = Profile.where("statut = ?", "Agent").limit(10)
-    end
+    # if user_signed_in? && current_user.ville != nil
+    #   ville = current_user.ville
+    #   # if Annonce.where("statut = ?", "active" ).near("#{ville}",30) == []
+    #   #   @annonces = Annonce.where("statut = ?", "active").order(created_at: :desc).limit(3)
+    #   # else
+    #   #   @annonces = Annonce.where("statut = ?", "active" ).near("#{ville}",30).limit(3)
+    #   # end
+    #   if Profile.where("statut = ?", "Agent" ).near("#{ville}",50) == []
+    #     @profiles = Profile.where("statut = ?", "Agent").limit(10)
+    #   else
+    #     @profiles = Profile.where("statut = ? AND id != ?", "Agent", current_user.id ).near("#{ville}",50).limit(10)
+    #   end
+    # else
+    # @annonces = Annonce.where("statut = ?", "active").order(created_at: :desc).limit(3)
+    @profiles = Profile.where("statut = ?", "Agent").limit(10)
+    # end
   end
 
   def new
