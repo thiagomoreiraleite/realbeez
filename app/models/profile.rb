4a57b3f006f8c1ad675429693e6464328702276b
steven ly <shiwenly@gmail.com>
2020-01-11 18:48:10 +0100

    search through statut

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,8 +1,8 @@
 class Profile < User
   include PgSearch::Model
   pg_search_scope :search_profile,
-    against: [ :ville, :adresse, :nom, :prénom ],
-    using: {
-      tsearch: { prefix: true }
-    }
+    against: [ :ville, :adresse, :nom, :prénom, :statut ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
